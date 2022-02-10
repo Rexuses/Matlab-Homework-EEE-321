53dd1dd00ec763f620291882da73444e1377b4a5
Rexuses <69196071+Rexuses@users.noreply.github.com>
2022-02-10 23:50:13 +0300

    Add files via upload

---
+++ Batuhan Matlab/bucket_sort.m
@@ -0,0 +1,22 @@
+%{
+16.10.2020
+Betül USLU
+170403004
+Bucket Sort Algorithm
+%}
+function array = bucket_sort(array)
+    %% Constants
+    max_array = max(array); %find max array
+    bucket = zeros(1,max_array+1); %create bucket
+   %% For Loop
+    for j = 1:numel(array)
+        bucket(array(j)) = bucket(array(j)) + 1;
+    end
+    index = 1;
+    for i = 1:max_array+1
+        for j = 1:bucket(i) 
+            array(index) = i;
+            index = index + 1;
+        end
+    end
+end
